f468d99166e3bb8344d4876be5230d3100ec6c95
Binlogo <binboy@live.com>
2021-03-16 09:12:24 +0800

    exercise: finish try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,8 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -25,19 +23,56 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        if (0..=255).contains(&red) && (0..=255).contains(&green) && (0..=255).contains(&blue) {
+            return Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            });
+        }
+        return Err("Check range".to_string());
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = arr[0];
+        let green = arr[1];
+        let blue = arr[2];
+        if (0..=255).contains(&red) && (0..=255).contains(&green) && (0..=255).contains(&blue) {
+            return Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            });
+        }
+        return Err("Check range".to_string());
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err("Check slice count, should be 3".to_string());
+        }
+        let red = slice[0];
+        let green = slice[1];
+        let blue = slice[2];
+        if (0..=255).contains(&red) && (0..=255).contains(&green) && (0..=255).contains(&blue) {
+            return Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            });
+        }
+        return Err("Check range".to_string());
+    }
 }
 
 fn main() {
